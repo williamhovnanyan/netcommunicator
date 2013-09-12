65408b49f2dc965175556e4428b55a59295b3b6c
William <william_93@mail.ru>
2013-09-12 22:48:09 +0400

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace lesson12_netcommuncator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.send1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonlisten = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.buttonconnect = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
+            this.buttonstop = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 224);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(45, 302);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(252, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // send1
+            // 
+            this.send1.Location = new System.Drawing.Point(322, 302);
+            this.send1.Name = "send1";
+            this.send1.Size = new System.Drawing.Size(75, 23);
+            this.send1.TabIndex = 2;
+            this.send1.Text = "Send";
+            this.send1.UseVisualStyleBackColor = true;
+            this.send1.Click += new System.EventHandler(this.send1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonlisten);
+            this.groupBox1.Controls.Add(this.maskedTextBox2);
+            this.groupBox1.Controls.Add(this.maskedTextBox1);
+            this.groupBox1.Location = new System.Drawing.Point(401, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(235, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server";
+            // 
+            // buttonlisten
+            // 
+            this.buttonlisten.Location = new System.Drawing.Point(97, 71);
+            this.buttonlisten.Name = "buttonlisten";
+            this.buttonlisten.Size = new System.Drawing.Size(117, 23);
+            this.buttonlisten.TabIndex = 4;
+            this.buttonlisten.Text = "Listen";
+            this.buttonlisten.UseVisualStyleBackColor = true;
+            this.buttonlisten.Click += new System.EventHandler(this.buttonlisten_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(6, 19);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(144, 20);
+            this.maskedTextBox1.TabIndex = 0;
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(157, 19);
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(57, 20);
+            this.maskedTextBox2.TabIndex = 1;
+            // 
+            // buttonconnect
+            // 
+            this.buttonconnect.Location = new System.Drawing.Point(97, 71);
+            this.buttonconnect.Name = "buttonconnect";
+            this.buttonconnect.Size = new System.Drawing.Size(117, 23);
+            this.buttonconnect.TabIndex = 4;
+            this.buttonconnect.Text = "Connect/Disconnect";
+            this.buttonconnect.UseVisualStyleBackColor = true;
+            this.buttonconnect.Click += new System.EventHandler(this.buttonconnect_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonstop);
+            this.groupBox2.Controls.Add(this.buttonconnect);
+            this.groupBox2.Controls.Add(this.maskedTextBox3);
+            this.groupBox2.Controls.Add(this.maskedTextBox4);
+            this.groupBox2.Location = new System.Drawing.Point(401, 165);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(235, 100);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Client";
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Location = new System.Drawing.Point(157, 19);
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.Size = new System.Drawing.Size(57, 20);
+            this.maskedTextBox3.TabIndex = 1;
+            // 
+            // maskedTextBox4
+            // 
+            this.maskedTextBox4.Location = new System.Drawing.Point(6, 19);
+            this.maskedTextBox4.Name = "maskedTextBox4";
+            this.maskedTextBox4.Size = new System.Drawing.Size(144, 20);
+            this.maskedTextBox4.TabIndex = 0;
+            // 
+            // buttonstop
+            // 
+            this.buttonstop.Location = new System.Drawing.Point(7, 71);
+            this.buttonstop.Name = "buttonstop";
+            this.buttonstop.Size = new System.Drawing.Size(75, 23);
+            this.buttonstop.TabIndex = 5;
+            this.buttonstop.Text = "Stop";
+            this.buttonstop.UseVisualStyleBackColor = true;
+            this.buttonstop.Click += new System.EventHandler(this.buttonstop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 349);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.send1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Net Communicator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button send1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonlisten;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button buttonconnect;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox4;
+        private System.Windows.Forms.Button buttonstop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lesson12_netcommuncator
+{
+    public partial class Form1 : Form
+    {
+        TcpClient client;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void send1_Click(object sender, EventArgs e)
+        {
+            if (client != null && client.Connected)
+            {
+                NetworkStream ns = client.GetStream();
+                byte[] bz=Encoding.Unicode.GetBytes(textBox2.Text);
+                IAsyncResult iar =
+                        ns.BeginRead(bz, 0, 1024, null, null);
+            }
+
+        }
+
+        
+        private void buttonlisten_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(Listening);
+            t.Start();
+
+        }
+        void Listening()
+        {
+            int port;
+            if (!int.TryParse(maskedTextBox2.Text, out port))
+                return;
+            TcpListener tListener = new TcpListener(new IPEndPoint(0x010007F, port));
+            tListener.Start();
+            client = tListener.AcceptTcpClient();
+            int idx;
+            while (client.Connected)
+            {
+                NetworkStream ns = client.GetStream();
+                byte[] buffer = new byte[1024];
+                IAsyncResult iar=
+                        ns.BeginRead(buffer, 0, 1024, null, null);
+                if (iar.AsyncWaitHandle.WaitOne(100))
+                {
+                    idx = ns.EndRead(iar);
+                    textBox1.Text = Encoding.Unicode.GetString(buffer);
+                }
+            }
+            tListener.Stop();
+ 
+        }
+        void Connecting()
+        {
+            int port;IPAddress addr;
+            if (!int.TryParse(maskedTextBox3.Text, out port) || 
+                !IPAddress.TryParse(maskedTextBox4.Text, out addr))
+                    return;
+
+            client = new TcpClient();
+            client.Connect(addr,port);
+            int idx;
+            byte[] buffer = new byte[1024];
+            while (client.Connected)
+            {
+                NetworkStream ns = client.GetStream();
+                
+                IAsyncResult iar =
+                        ns.BeginRead(buffer, 0, 1024, null, null);
+                if (iar.AsyncWaitHandle.WaitOne(100))
+                {
+                    idx = ns.EndRead(iar);
+                    textBox1.Text = Encoding.Unicode.GetString(buffer);
+                }
+            }
+        }
+        private void buttonconnect_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(Connecting);
+            t.Start();
+        }
+
+        private void buttonstop_Click(object sender, EventArgs e)
+        {
+            if (client != null && client.Connected)
+            {
+                client.Close();
+            }
+        }
+    }
+}
